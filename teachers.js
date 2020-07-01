f66a1670dab5f29d043c16e76e5699a0b096041c
Gustavo Raeski <gustavoraeski@outlook.com>
2020-07-01 14:51:10 -0300

    Configurando data.json para receber os dados

--- teachers.js
+++ teachers.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const data = require('./data.json')
 //CREATE
     exports.post = function(req,res){
         //return res.send('recebido') //Recebendo os dados do create.njk
@@ -9,12 +10,16 @@ const fs = require('fs')
             if(req.body[key] == "")
                 return res.send('Please, fill all fields!')
         }
-        fs.writeFile("data.json", JSON.stringify(req.body), function (err) {
+
+        data.teachers.push(req.body)
+
+        fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
             if(err) return res.send("Write file error!")
+                    
             return res.redirect("/teachers")
         })
 
-        return res.send(keys)
+        //return res.send(keys)
     }
 
 
