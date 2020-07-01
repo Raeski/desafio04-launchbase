d99091c5dcd015a05bacd006b0cb2013b8abb9b0
Gustavo Raeski <gustavoraeski@outlook.com>
2020-07-01 14:21:20 -0300

    Configurando express, para aceitar dados do front

--- routes.js
+++ routes.js
@@ -13,7 +13,9 @@ routes.get('/teachers/create', function(req,res) {
     return res.render("teachers/create")
 })
 routes.post("/teachers", function(req,res){
-    return res.send('recebido') //Recebendo os dados do create.njk
+    //return res.send('recebido') //Recebendo os dados do create.njk
+
+    return res.send(req.body)
 })
 
 routes.get('/students', function (req,res){
