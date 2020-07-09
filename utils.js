e658b855560305eb0dd5dc0bf22461210800af81
Gustavo Raeski <gustavoraeski@outlook.com>
2020-07-09 13:57:35 -0300

    Creando function para aplicar idade

---
+++ utils.js
@@ -0,0 +1,19 @@
+module.exports = {
+    age:function (timestamp) {
+        const today = new Date()
+        const birthDate = new Date(timestamp)
+    
+        // 2020 - 1999 = 21
+        // 11 - 11 = 0
+        let age =  today.getFullYear() - birthDate.getFullYear()
+        const month = today.getMonth() - birthDate.getMonth()
+    
+
+        if (month < 0 || month == 0 && today.getDate() <  birthDate.getDate()){
+            age = age -1
+        }
+    
+        return age
+    
+    }
+}
