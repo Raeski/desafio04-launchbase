df3f02e5222654338f4121ecee089485f50f6d82
Gustavo Raeski <gustavoraeski@outlook.com>
2020-07-20 17:44:53 -0300

    Ultimas estilizaçoes na tabela

--- teachers.js
+++ teachers.js
@@ -106,7 +106,8 @@ exports.show = function(req,res) {
         const teacher = {
             ...foundTeacher,
             ...req.body,
-            birth: Date.parse(req.body.birth)
+            birth: Date.parse(req.body.birth),
+            id: Number(req.body.id)
         }
 
         data.teachers[index] = teacher
