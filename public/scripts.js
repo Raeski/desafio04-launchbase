bce6d1d90f5d77c0666138606b5244babb7103dc
Gustavo Raeski <gustavoraeski@outlook.com>
2020-07-22 11:44:40 -0300

    Menu ativo

--- public/scripts.js
+++ public/scripts.js
@@ -1,15 +1,9 @@
-const modalOverlay = document.querySelector('.modal-overlay');
-const cards = document.querySelectorAll('.card')
+const currentPage = location.pathname
+const menuItems = document.querySelectorAll("header .links a")
+console.log(currentPage)
 
-for(let card of cards ){
-    card.addEventListener("click", function(){
-        modalOverlay.classList.add("active")
-        modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`
-    })
+for(item of menuItems) {
+    if(currentPage.includes(item.getAttribute("href"))) {
+        item.classList.add("active")
+    }
 }
-
-closeModal = document.querySelector(".close-modal").addEventListener("click", function(){
-    modalOverlay.classList.remove("active")
-})
-
-
