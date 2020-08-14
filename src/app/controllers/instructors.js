f173f81eceeb3638fd747cea334984e51321afe7
Gustavo Raeski <gustavoraeski@outlook.com>
2020-08-14 14:36:37 -0300

    Começando alteraçao de dados para fazer requisiçoes ao banco de dados

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,9 +1,14 @@
-const { age, date } = require('../../lib/utils')
+const Instructor = require('../models/Instructor')
+const { age, date} = require('../../lib/utils')
 
 module.exports = {
 
-    index(req,res) {
-    return res.render("instructors/index",)},
+index(req,res) {
+    Instructor.all(function(instructors) {
+            return res.render("instructors/index", {instructors})
+    })
+},
+
     create(req,res) {
         return res.render('instructors/create')
     },
@@ -17,29 +22,24 @@ module.exports = {
         return res.send('Please, fill all fields')
     }
 
-    let {avatar_url, birth, name, services, gender} = req.body
-
-    return
+    Instructor.create(req.body, function(instructor) {
+        return res.redirect(`/instructors/${instructor.id}`)
+    })
    
     },
     show(req,res) {const { id } = req.params
+        Instructor.find(req.params.id, function(instructor) {
+            if(!instructor)  return res.send("Instructor not found!")
 
-    const foundInstructor = data.instructors.find(function (instructor){
-        return id == instructor.id
-    })
-    if(!foundInstructor)  return res.send("Instructor not found!") 
+            instructor.age = age(instructor.birth)
+            instructor.services = instructor.services.split(",")
 
-   
-   
-    
-    const instructor = {
-        ...foundInstructor,
-        age: age(foundInstructor.birth),
-        services: foundInstructor.services.split(","),
-        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
-    }
+            instructor.created_at = date(instructor.created_at).format
 
-    return res.render("instructors/show", {instructor})},
+            return res.render("instructors/show", {instructor})
+        })
+    },
+
     edit(req,res) {
         return
     },
@@ -49,4 +49,4 @@ module.exports = {
     delete(req,res) {
         return
     },
-}
+    }
