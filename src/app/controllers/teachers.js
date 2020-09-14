9e2153085b85a2a2f32c12ad0768dd897b877900
Gustavo Raeski <gustavoraeski@outlook.com>
2020-09-14 13:41:59 -0300

    Adicionando metodo de UPDATE

--- src/app/controllers/teachers.js
+++ src/app/controllers/teachers.js
@@ -1,4 +1,4 @@
-
+const {age, date} = require('../../lib/utils')
 const Teacher = require('../models/Teacher')
 
 module.exports = {
@@ -25,21 +25,39 @@ module.exports = {
     },
 
     show(req,res){
-        return
+        Teacher.find(req.params.id, function(teacher) {
+            if(!teacher) return res.send("Teacher not found!")
+
+            teacher.age = age(teacher.birth)
+            teacher.services = teacher.services.split(",")
+            teacher.created_at = date(teacher.created_at).format
+
+            return res.render("teachers/show", { teacher })
+
+        })
     },
 
     edit(req,res){
-        return
+        Teacher.find(req.params.id, function(teacher) {
+            if(!teacher) return res.send("Teacher not found!")
+
+            teacher.birth = age(teacher.birth)
+            return res.render("teachers/edit", { teacher })
+
+        })
     },
 
     put(req,res){
         const keys = Object.keys(req.body) 
 
         for (key of keys){
-            if(req.body[key] == "")
+            if(req.body[key] == "") {
                 return res.send('Please, fill all fields!')
         }
-        return
+    }
+        Teacher.update(req.body, function () {
+            return res.redirect(`/instructors/${req.body.id}`)
+        })
     },
     
     delete(req,res){
--- src/app/models/Teacher.js
+++ src/app/models/Teacher.js
@@ -1,4 +1,4 @@
-const {age, date} = require('../../lib/utils')
+const {date} = require('../../lib/utils')
 
 const db = require('../../config/db')
 
@@ -19,8 +19,9 @@ module.exports = {
         birth,
         gender,
         services,
-        degree
-    ) VALUES ($1, $2, $3, $4, $5, $6)
+        degree,
+        created_at
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7)
     RETURNING id
 `
 
@@ -28,6 +29,7 @@ module.exports = {
         data.avatar_url,
         data.name,
         date(data.birth).iso,
+        date(Date.now()).iso,
         data.gender,
         data.services,
         data.degree
@@ -38,4 +40,41 @@ module.exports = {
         callback(results.rows[0])
     })
   },
+  find(id, callback) {
+    db.query(`
+    SELECT * 
+    FROM teachers 
+    WHERE id = $1`, [id], function(err, results) {
+      if(err) return res.send("Database Error!")
+      callback(results.rows[0])
+    })
+  },
+  update(data, callback) {
+    const query = `
+    UPDATE instructors SET
+      avatar_url=($1),
+      name=($2),
+      birth=($3),
+      gender=($4),
+      services=($5),
+      degree=($6)
+    WHERE id = $6
+    `
+    const values = [
+      data.avatar_url,
+      data.name,
+      date(data.birth).iso,
+      data.gender,
+      data.services,
+      data.degree,
+      data.id
+
+    ]
+    db.query(query, values, function (err, results) {
+      if(err) return res.send("Database Error!")
+
+        callback()
+    })
+  }
+
 }
